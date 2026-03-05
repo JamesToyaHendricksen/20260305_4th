6e33bc0d5c264efcbcb8b667acb44f5074522a8b
LAPTOP-AMQI5BOG\academia <202511_aw_toyoda@x-ide.sakura.ne.jp>
2026-03-05 15:12:24 +0900

    Spring Boot + MyBatisでToDoの削除機能を実装

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -4,6 +4,7 @@ import com.example.demo.service.TodoService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -38,6 +39,12 @@ public class TodoController {
     @PostMapping("/complete")
     public String complete(@RequestParam("title") String title) {
         todoService.createTodo(title);
+        return "redirect:/todo";
+    }
+
+    @PostMapping("/{id}/delete")
+    public String delete(@PathVariable("id") Long id) {
+        todoService.deleteById(id);
         return "redirect:/todo";
     }
 }
